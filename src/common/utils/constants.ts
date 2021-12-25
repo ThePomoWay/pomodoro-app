706272b75ba4d094ea69c771bd70024181f59acc
Mukesh P <mukeshp6295@gmail.com>
2021-12-25 09:45:32 +0530

    UI fixes

--- src/common/utils/constants.ts
+++ src/common/utils/constants.ts
@@ -11,7 +11,7 @@ export const POMO_LONG_BREAK_PAUSED_STATE   = 'pomo_long_break_paused';
 export const allTasksDropId = 'id-2e';
 export const todaysTasksDropId = 'id-1e';
 
-export let DEFAULT_WORK_TIME        = 1 * 10;
+export let DEFAULT_WORK_TIME        = 25 * 60;
 export let DEFAULT_BREAK_TIME       = 5 * 60;
 export let DEFAULT_LONG_BREAK_TIME  = 15 * 60;
 
