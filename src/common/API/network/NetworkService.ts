cc3007ab7e4b59eacf9ecd1e02d6961576d18ced
emer <masirkarakash@gmail.com>
2022-03-11 17:06:31 +0530

    minor fixes

--- src/common/API/network/NetworkService.ts
+++ src/common/API/network/NetworkService.ts
@@ -32,7 +32,7 @@ function throwNetworkErrorToast(message) {
     setToast({
       open: true,
       msg: message || "Please check your internet connection",
-      duration: 5000,
+      duration: 2000,
       type: "failure",
     })
   );
@@ -60,7 +60,6 @@ export class NetworkService {
       .then((res) => {
         if (!res || res.status !== 200) {
           throwNetworkErrorToast();
-          Promise.reject();
           return res;
         }
         syncSuccessful(res.data.mapFIDToTID);
