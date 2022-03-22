307a18892c25f21c6daf104356ce2606b2fd6c9d
Mukesh P <mukeshp6295@gmail.com>
2022-03-22 21:43:57 +0530

    Adding support for GA

--- src/usePageTracking.ts
+++ src/usePageTracking.ts
@@ -8,18 +8,30 @@ const usePageTracking = () => {
 
   useEffect(() => {
     if (window) {
-      if (!window.location.href.includes("beta")) {
-        ReactGA.initialize("UA-000000000-0");
-      } else {
-        ReactGA.initialize("UA-212261258-1");
-      }
+      // if (!window.location.href.includes("beta")) {
+      //   ReactGA.initialize("UA-000000000-0");
+      // } else {
+      //   ReactGA.initialize("UA-212261258-1");
+      // }
       setInitialized(true);
     }
   }, []);
 
   useEffect(() => {
-    if (initialized) {
-      ReactGA.pageview(location.pathname + location.search);
+    if (initialized && window.gtag) {
+      // ReactGA.pageview(location.pathname + location.search);
+      let pageTitle = "HomePage";
+      if (location.pathname.startsWith("/all")) {
+        pageTitle = "AllPage";
+      }
+      if (location.pathname.startsWith("/analysis")) {
+        pageTitle = "AnalysisPage";
+      }
+      window.gtag("event", "page_view", {
+        page_title: pageTitle,
+        page_location: location.href,
+        page_path: location.pathname,
+      });
     }
   }, [initialized, location]);
 };
